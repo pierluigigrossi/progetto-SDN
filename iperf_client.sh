4a314c2b6391123381d25fa9de08959837181382
Pierluigi Grossi <grossi.p00gmail.com>
2024-04-04 22:38:53 +0200

    iperf

--- iperf_client.sh
+++ iperf_client.sh
@@ -1,13 +1,16 @@
-#!/bin/sh
-ip_server = localhost
-port = 5201
+#!/bin/bash
+ip_server=localhost
+port=5201
 t_min=10
 t_max=30
-sleep_min= 0.5
-sleep_max= 5 
+parallel_min=1
+parallel_max=1
+sleep_min=0.5
+sleep_max=5 
 while true; do
-    rand2=$(($RANDOM%($sleep_max-$sleep_min+1)+$sleep_min))
-    rand=$(($RANDOM%($t_max-$t_min+1)+$t_min))
-    iperf3 -c $ip_server -port $port -t  $rand -p 
-    sleep $rand2
+    parallel=$(($RANDOM%(parallel_max-$parallel_min+1)+$parallel_min))
+    wait=$(($RANDOM%($sleep_max-$sleep_min+1)+$sleep_min))
+    t=$(($RANDOM%($t_max-$t_min+1)+$t_min))
+    iperf -c $ip_server -port $port -t  $t -p $parallel
+    sleep $wait
 done
