eef68828d1e2ffb335c1338a31545cbd55f00be6
Pierluigi Grossi <grossi.p00gmail.com>
2024-04-05 09:10:49 +0200

    delete corretta

--- tcp.py
+++ tcp.py
@@ -126,10 +126,10 @@ class HopByHopSwitch(app_manager.RyuApp):
                     delta_t = delta_t + d[destination_mac][l-1]-d[destination_mac][l-2]
                     l = l-1
                     i = i+1
-                l = len(d[destination_mac])
                 #tieni nel dizionario solo SYN nell'intervallo di tempo di osservazione 0-T
-                if  l > 0  and delta_t > T :
-                    del d[destination_mac]
+                while  l > 0  :
+                    del d[destination_mac][l-1]
+                    l = l-1
                 #scarta pacchetto oltre soglia, più di X SYN in tempo minore di T
                 if i > X  and delta_t < T:
                    print('KO\n')
