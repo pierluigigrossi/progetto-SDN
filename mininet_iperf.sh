d60e3479951ed3135e9d616153c29f40ef40852a
Pierluigi Grossi <grossi.p00gmail.com>
2024-04-22 10:08:44 +0200

    retries

--- mininet_iperf.sh
+++ mininet_iperf.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "SYN prima di timeoute"
+cat /proc/sys/net/ipv4/tcp_synack_retries 
 echo "Partenza  automatica sui 4 host degli script"
 cd /root/sdn-labs/progetto8
 /root/mininet/util/m h1 ./iperf.sh &
