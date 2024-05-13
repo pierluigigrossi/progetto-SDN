908372435475f8eb8926aa3f7414c66cc27f3a90
Pierluigi Grossi <grossi.p00gmail.com>
2024-05-13 22:17:14 +0200

    last

--- thumb_h1.sh
+++ thumb_h1.sh
@@ -3,9 +3,9 @@ echo "TCPDUMP su SYN RST FIN"
 tcpdump "tcp[tcpflags] & (tcp-syn|tcp-fin|tcp-rst) != 0" -w  h1_thumb.pcap &
 echo "iperf verso h2"
 sleep 3
-iperf -c 10.0.0.2
-iperf -c 10.0.0.2
-iperf -c 10.0.0.2
+iperf -c 10.0.0.2 
+iperf -c 10.0.0.2 
+iperf -c 10.0.0.2 
 iperf -c 10.0.0.2
 sleep 5
 killall tcpdump
