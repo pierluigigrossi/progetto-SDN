06a9be1a88c009840afd68ae2138556c2b86e242
Pierluigi Grossi <grossi.p00gmail.com>
2024-05-17 22:40:53 +0200

    post test

---
+++ risultati/thumb rst/thumb_h1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "TCPDUMP su SYN RST FIN"
+sudo tcpdump "tcp[tcpflags] & (tcp-syn|tcp-fin|tcp-rst) != 0" -i eth0  -w  h1_thumb.pcap &
+echo "iperf verso h2"
+sleep 3
+iperf -c 10.10.6.41 -p 5241 -t 1
+iperf -c 10.10.6.41 -p 5241 -t 1
+iperf -c 10.10.6.41 -p 5241 -t 1
+iperf -c 10.10.6.41 -p 5241 -t 1
+iperf -c 10.10.6.41 -p 5241 -t 1
+sleep 5
+sudo killall tcpdump
